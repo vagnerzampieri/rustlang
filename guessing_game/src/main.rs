e52bd24059ed9a57a7b6af300aa97e92ac184d85
Vagner Zampieri <vgzampieri@gmail.com>
2023-01-01 21:56:46 -0300

    Identation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,8 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng()
+                              .gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
@@ -17,7 +18,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = guess.trim()
+                          .parse()
+                          .expect("Please type a number!");
 
     println!("You guessed: {guess}");
 
