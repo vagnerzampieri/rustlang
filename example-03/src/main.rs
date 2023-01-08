2ceb31d2517c35279cd93d0336d9941909d92513
Vagner Armond Zampieri <vgzampieri@gmail.com>
2023-01-08 10:51:01 -0300

    Create function

--- example-03/src/main.rs
+++ example-03/src/main.rs
@@ -1,18 +1,20 @@
 use std::fs;
 
+fn sum_data(block: &str) -> u32 {
+    block
+        .split('\n')
+        .filter(|item| !item.is_empty())
+        .map(|item| item.parse::<u32>().unwrap())
+        .sum::<u32>()
+}
+
 fn main() {
     let contents = fs::read_to_string("data.txt")
         .expect("Cannot open to the file.");
 
     let result: u32 = contents
         .split("\n\n")
-        .map(|block| 
-            block
-                .split('\n')
-                .filter(|item| !item.is_empty())
-                .map(|item| item.parse::<u32>().unwrap())
-                .sum::<u32>()
-        )
+        .map(sum_data)
         .max()
         .unwrap();
 
