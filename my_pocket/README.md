afe046667618c62d8fb90c68d843897337a43945
Vagner Zampieri <vagner.zampieri@nuuvem.com.br>
2023-09-03 21:05:17 -0300

    Add mongo

---
+++ my_pocket/README.md
@@ -0,0 +1,82 @@
+# My Pocket
+
+## Description
+
+This is a simple app to save your favorite links with a description and a tag. It's possible to remove the link, edit the description and the tag.
+
+## Models
+
+Pocket
+
+```
+{
+  description: String,
+  link: String,
+  tags: [Tag]
+}
+```
+
+Tag
+
+```
+{
+  name: String
+}
+```
+## API
+
+GET `/pockets?tag=tag_name`
+```
+[
+  {
+    description: String,
+    link: String,
+    tags: [Tag]
+  }
+]
+```
+
+GET `/pockets/:id`
+```
+{
+  description: String,
+  link: String,
+  tags: [Tag]
+}
+```
+
+POST `/pockets`
+```
+{
+  description: String,
+  link: String,
+  tags: [Tag]
+}
+```
+
+PUT `/pockets/:id`
+```
+{
+  description: String,
+  link: String,
+  tags: [Tag]
+}
+```
+
+DELETE `/pockets/:id`
+```
+{
+  description: String,
+  link: String,
+  tags: [Tag]
+}
+```
+
+GET `/tags`
+```
+[
+  {
+    name: String
+  }
+]
+```
--- my_pocket/src/main.rs
+++ my_pocket/src/main.rs
@@ -1,8 +1,43 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use dotenv::dotenv;
+use mongodb::{Client, Database, bson, bson::doc, bson::oid::ObjectId, error::Result};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tag {
+    #[serde(rename = "_id")]
+    id: ObjectId,
+    name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Pocket {
+    #[serde(rename = "_id")]
+    id: ObjectId,
+    description: String,
+    link: String,
+    tags: Vec<Tag>,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+}
 
 #[get("/pockets")]
 async fn pockets() -> impl Responder {
-    HttpResponse::Ok().body("List of links")
+    let db = db().await.unwrap();
+    let collection = db.collection::<Pocket>("pockets");
+    let cursor = collection.find(doc! {}, None).await.unwrap();
+    let mut pockets: Vec<Pocket> = Vec::new();
+
+    for result in cursor {
+        if let Ok(document) = result {
+            let pocket: Pocket = bson::from_bson(bson::Bson::Document(document)).unwrap();
+            pockets.push(pocket);
+        }
+    }
+
+    HttpResponse::Ok().json(pockets)
 }
 
 #[post("/create_pocket")]
@@ -15,8 +50,16 @@ async fn pocket_by_id(id: web::Path<u32>) -> impl Responder {
     HttpResponse::Ok().body(format!("Pocket with id: {}", id))
 }
 
+async fn db() -> Result<Database> {
+    let client = Client::with_uri_str(&env::var("MONGO_URI").unwrap()).await.unwrap();
+    let db: Database = client.database(&env::var("MONGO_DB").unwrap());
+    Ok(db)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     HttpServer::new(|| {
         App::new()
             .service(pockets)
